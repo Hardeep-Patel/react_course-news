e180df402c9dfe7d5b57af6caf043a346cf39794
Hardeep-Patel <haripatel2303@gmail.com>
2025-06-03 12:50:09 +0530

    done

--- newsapp/src/component/NewsItem.js
+++ newsapp/src/component/NewsItem.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
+import { data } from 'react-router-dom';
 
 export class NewsItem extends Component {
   
   render() {
-      let {title, description , imageUrl ,newsUrl} = this.props;
+      let {title, description , imageUrl ,newsUrl , author ,data} = this.props;
 
     return (
       <div>
@@ -12,6 +13,7 @@ export class NewsItem extends Component {
             <div className="card-body">
               <h5 className="card-title">{title}</h5>
               <p className="card-text">{description}</p>
+              <p className="card-text"><small className="text-muted">By {author} on {data}</small></p>
               <a rel="noreferrer" href={newsUrl} target='_blank' className="btn btn-sm btn-dark">Read More</a>
             </div>
         </div>
